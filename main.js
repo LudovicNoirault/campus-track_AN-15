16ee4243ce2ae06ae28cc20ac72568b942fafcd8
elfepee <tristan.meillat28@gmail.com>
2020-04-02 16:38:36 +0100

    ajout style menu

--- main.js
+++ main.js
@@ -38,6 +38,5 @@ function createWindow () {
       createWindow()
     }
   })
-  
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
--- seaLevel.php
+++ seaLevel.php
@@ -10,6 +10,10 @@
 <?php
   $seaLevel = 25;
 ?>
+
+<div class="slidecontainer">
+  <p>Default range slider:</p>
+  <input type="range" min="1" max="100" value="50">
 <iframe style="width:600px;height:600px"
    src="http://flood.firetree.net/?ll=46.9533,5.3696&m=<?php $seaLevel ?>"
    scrolling="no" frameborder="no" marginheight="0"></iframe>
