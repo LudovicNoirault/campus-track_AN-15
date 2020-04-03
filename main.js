5f897efe42fc7b0970f19633646b2e06b97d798d
Ludovic NOIRAULT <ludovic.noirault@imie.fr>
2020-04-03 09:18:36 +0200

    Changes to the map to make it more readable and useful plus fix slider

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
     // Create the browser window.
     const win = new BrowserWindow({
-      width: 800,
-      height: 960,
+      width: 1280,
+      height: 720,
       webPreferences: {
         nodeIntegration: true
       }
@@ -12,9 +12,6 @@ function createWindow () {
   
     // and load the index.html of the app.
     win.loadFile('index.html')
-  
-    // Open the DevTools.
-    win.webContents.openDevTools()
   }
   
   // This method will be called when Electron has finished
--- system.js
+++ system.js
@@ -1,8 +1,28 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 47.4474629, lng: -0.5421491},
+    zoom: 8,
+    disableDefaultUI: true,
+    mapTypeId: 'hybrid',
+      styles: [
+        {
+        featureType: "road",
+        stylers: [
+                 {visibility: "off"}
+             ]
+        }
+    ]
   });
 }
 
+
+const slider = document.getElementById('slider')
+const sliderValue = document.getElementById('sliderValue')
+// init value to display
+sliderValue.innerHTML = slider.value;
+
+// Then do it each time the slider change 
+slider.onchange = () => {
+    sliderValue.innerHTML = slider.value;
+}
