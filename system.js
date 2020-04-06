826016e915ab4e36ea8975953087d14d92cab8fc
Ludovic NOIRAULT <ludovic.noirault@imie.fr>
2020-04-06 10:42:36 +0200

    able to streetview on zoom yoohoo

--- system.js
+++ system.js
@@ -1,5 +1,7 @@
 var map;
-function initMap() {
+var street = undefined;
+
+function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.4474629, lng: -0.5421491},
     zoom: 8,
@@ -12,10 +14,37 @@ function initMap() {
                  {visibility: "off"}
              ]
         }
-    ]
-  });
-}
+      ]
+    }
+  )
+};
 
+var t=setInterval(mapState,1000);
+
+function mapState(){
+  console.log("zoom level", map.zoom)
+  if(map.zoom > 19){
+    map.mapTypeId = "satellite"
+    
+    if(!street){
+
+      console.log("new street")
+
+      street = new google.maps.StreetViewPanorama(
+        document.getElementById('map'), {
+          position: map.center,
+          pov: {
+            heading: 34,
+            pitch: 10
+          }
+        });
+       map.setStreetView(street);
+    }
+  }
+  else{
+    map.mapTypeId = 'hybrid'
+  }
+};
 
 $(function() {
 	$('.range').next().text('2020'); // Valeur par défaut
