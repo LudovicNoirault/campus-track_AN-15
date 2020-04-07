d135a268e5bb1109704b9c9f5726ca6fae4a7e3b
Ludovic NOIRAULT <ludovic.noirault@imie.fr>
2020-04-07 11:59:04 +0200

    removed unused jquery, rework html and implemented system to previous overlay

--- system.js
+++ system.js
@@ -2,6 +2,9 @@ var map
 var street = undefined
 var startingPos = {lat: 47.4474629, lng: -0.5421491}
 var elevator
+var elevation
+var waterLevelLimit = 20
+var originalYear = 2020
 
 // Initial map with custom pos
 function initMap(){
@@ -76,31 +79,83 @@ function enableStreetView(){
       }
     }
   );
+
+  //
+  getElevation(map, elevator) 
+
   // set street view on map where we defined it
   map.setStreetView(street);
 }
 
-
 function getElevation(map, elevator){
-  // wait 1 second before to call to prevent overpricing by calling for each micro change of the map
-  setTimeout(function () {
-    elevator.getElevationForLocations({
-      'locations': [map.center]
-    }, function(results, status) {
-      console.log("elevation", results)
-    });
-  }, 1000);
+  // wait 0.5 second before calling elavation api to prevent overpricing by calling for each micro change of the map
+  if(street){
+    setTimeout(function () {
+      elevator.getElevationForLocations({
+        'locations': [map.center]
+      }, function(results, status) {
+        console.log("hey", results)
+        elevation = results[0].elevation
+      });
+    }, 500);
+  }
+}
+
+function sliderAction(value){
+  document.getElementById("sliderYear").innerHTML = originalYear + value
+  var overlay = document.getElementById("overlay")
+
+  if(street && elevation <= waterLevelLimit){
+    console.log("we doing good")
+    console.log("before", overlay.style.height)
+    overlay.style.height = value+"%"
+    console.log("after", overlay.style.height)
+  }
 }
 
 // var valueSlider = document.getElementById("slider").value;
 // valueSlider = (2120 - valueSlider) / 10;
 // document.getElementById("test").innerHTML = valueSlider;
 
-// Slider
-$(function() {
-  $('.range').next().text('2020'); // Valeur par défaut
-	$('.range').on('input', function() {
-    var set = $(this).val();
-		$(this).next().text(set);
-	});
-});
+// // Slider
+// $(function() {
+//   $('.range').next().text('2020'); // Valeur par défaut
+// 	$('.range').on('input', function() {
+//     var set = $(this).val();
+// 		$(this).next().text(set);
+// 	});
+// });
+
+// $(window).on('ready', function () {
+//   // working slider   
+//   $('#slider').slider()
+  
+//   var startPos = $("#slider").slider("value");
+//   var endPos = '';
+  
+//   $("#slider").on("slidestop", function(event, ui) {
+//         endPos = ui.value;
+
+//         if (startPos != endPos) {
+//           $("#sliderYer").innerHtml = endPos
+          
+//           if(street && elevation <= waterLevelLimit){
+//             var h = $('.range').val()
+            
+//             console.log("range val", h)
+//             $(".overlay")
+//               .css('height', h);
+//           }
+//         }
+//         startPos = endPos;
+//     });
+// // }
+
+// //   $.on('input', function() {
+// //     var set = $(this).val();
+// //     $(this).next().text(set);
+
+// //     console.log('test', street)
+// //     // If we are in streetview and under 11 meters aboves the water level 
+// //   });
+// });
